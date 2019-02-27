a516fcfd7c4f249111fe1c4c741eaf14c16b20bd
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-02-27 20:12:44 +0100

    Some cleanup work from generated code via Nuxt (#1)
    * cleanup default view
    added settingd button to toggle dark mode. more settings to come
    
    * cleanup pages
    
    * remove clickevent. with it the switch was not clickable
    
    * cleanup component and test spec

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app :dark="darkTheme">
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
@@ -30,57 +30,40 @@
       app
     >
       <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <v-menu bottom left nudge-left="20" nudge-bottom="20" :close-on-content-click="false">
+        <v-btn
+          slot="activator"
+          icon
+        >
+          <v-icon>settings</v-icon>
+        </v-btn>
+        <v-list two-line subheader>
+          <v-subheader>Settings</v-subheader>
+          <v-list-tile avatar>
+            <v-list-tile-content>
+              <v-list-tile-title> To be defined</v-list-tile-title>
+              <v-list-tile-sub-title>TO be defined</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-subheader>Customizations</v-subheader>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-switch v-model="darkTheme" color="primary" />
+            </v-list-tile-action>
+            <v-list-tile-title>Dark theme</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>
-              compare_arrows
-            </v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
-      :fixed="fixed"
+      :fixed="fixedFooter"
       app
     >
       <span>&copy; 2019</span>
@@ -92,25 +75,23 @@
 export default {
   data() {
     return {
-      clipped: false,
+      clipped: true,
       drawer: false,
-      fixed: false,
+      fixedFooter: true,
+      darkTheme: false,
       items: [
         {
           icon: 'apps',
-          title: 'Welcome',
+          title: 'Home',
           to: '/'
         },
         {
           icon: 'bubble_chart',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'About',
+          to: '/about'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Color Splash'
     }
   }
 }
