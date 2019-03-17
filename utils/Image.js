aa322462e6cc0b61e0ad3c20d427b5419a8f1e78
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-03-17 14:47:37 +0100

    Update Jest to 24.5.0 And Image class to parse raw unsplash image objects (#6)
    * Update to latest jest version since it had a bug with my current node version
    
    * Added Image class with some methods and test for the class. Added test data to work with

---
+++ utils/Image.js
@@ -0,0 +1,71 @@
+/**
+ * Class representing Unsplash image object
+ */
+class Image {
+  /**
+   * Class constructor
+   * @param {Object} input Raw Unsplash image object
+   */
+  constructor(input) {
+    // Assign passed input to class context
+    Object.assign(this, input)
+  }
+
+  /**
+   * Get raw image url
+   * @returns {String} url
+   */
+  getImageRaw() {
+    return this.urls.raw
+  }
+
+  /**
+   * Get full size image url
+   * @returns {String} url
+   */
+  getImageFull() {
+    return this.urls.full
+  }
+
+  /**
+   * Get regular size image url
+   * @returns {String} url
+   */
+  getImageRegular() {
+    return this.urls.regular
+  }
+
+  /**
+   * Get small size image url
+   * @return {String} url
+   */
+  getImageSmall() {
+    return this.urls.small
+  }
+
+  /**
+   * Get thumbnail size image url
+   * @returns {String} url
+   */
+  getImageThumb() {
+    return this.urls.thumb
+  }
+
+  /**
+   * Get user info
+   * @returns {Object}
+   */
+  getUserInfo() {
+    return this.user
+  }
+
+  /**
+   * Get users first and last name in a formatted string
+   * @returns {String}
+   */
+  getUserNameFormatted() {
+    return `${this.user.first_name} ${this.user.last_name}`
+  }
+}
+
+export default Image
