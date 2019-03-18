a45624683c32333664db2b6cebd3cbf907ae0cfd
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-03-18 18:56:48 +0100

    Add ImagePalette class to determine dominant colors of an image (#7)
    * Added Chroma and node-vibrant packages to extract dominant colors from images
    
    * Added ImagePalette class and testing

---
+++ utils/ImagePalette.test.js
@@ -0,0 +1,34 @@
+import ImagePalette from './ImagePalette'
+import rawImage from './testdata/image.json'
+
+const imagePalette = new ImagePalette(rawImage)
+
+describe('ImagePalette class', () => {
+  it('should be defined', () => {
+    expect(ImagePalette).toBeDefined()
+  })
+
+  it('should properly have extended Image', () => {
+    const inheritedMethods = ['getImageFull', 'getUserInfo', 'getImageThumb']
+    inheritedMethods.map(method => expect(imagePalette[method]).toBeDefined())
+  })
+
+  it('should get dominant colors from an image', async () => {
+    const colors = await imagePalette.getDominantColors()
+    expect(colors.length).toBeGreaterThan(0)
+  })
+
+  it('should get dominant colors in hex format', async () => {
+    const colors = await imagePalette.getDominantColors()
+    expect(colors[0].includes('#')).toBeTruthy()
+  })
+
+  it('should get dominant colors as Chroma instances', async () => {
+    const colors = await imagePalette.getChromaColors()
+    const color = colors[0]
+    // Test that colors has inherited chroma prototype
+    expect(color.hex).toBeDefined()
+    expect(color.rgb).toBeDefined()
+    expect(color.saturate).toBeDefined()
+  })
+})
