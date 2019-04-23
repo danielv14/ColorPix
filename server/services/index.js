b864d0c104d55c4d67d9a5106b376ec540fbef04
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-04-23 20:31:13 +0200

    Added mockData service

--- server/services/index.js
+++ server/services/index.js
@@ -1,9 +1,11 @@
 const imageRandomService = require('./imageRandom.service')
 const imagesSearchService = require('./imagesSearch.service')
 const imageListService = require('./imageList.service')
+const mockDataService = require('./mockData.service')
 
 module.exports = {
   imageRandomService,
   imagesSearchService,
-  imageListService
+  imageListService,
+  mockDataService
 }
