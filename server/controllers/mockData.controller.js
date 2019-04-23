ea6be4c07672eb488817450a0de949eac4f1a65d
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-04-23 20:32:39 +0200

    Added mockdata controller and routing for mocking image list response

---
+++ server/controllers/mockData.controller.js
@@ -0,0 +1,21 @@
+const { mockDataService } = require('../services')
+const { mockDataImageList } = mockDataService
+
+/**
+ * Get a mock response with list of 10 images
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ */
+const getMockImageList = (req, res, next) => {
+  try {
+    const data = mockDataImageList()
+    res.send(data)
+  } catch (e) {
+    res.sendStatus(500) && next(e)
+  }
+}
+
+module.exports = {
+  getMockImageList
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,10 +5,16 @@ const {
   getImageList
 } = require('../controllers')
 
+// Require methods from mockData controller seperatly since controllers index does not require and export it
+const { getMockImageList } = require('../controllers/mockData.controller')
+
 const router = express.Router()
 
 router.get('/image/random', getImageRandom)
 router.get('/images/search', getImagesByKeyword)
 router.get('/images/list', getImageList)
+
+// Setup mock route for image list response
+router.get('/mock/list', getMockImageList)
 
 module.exports = router
