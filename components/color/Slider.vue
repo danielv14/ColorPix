a78f573e4b2a987a253984d619095339fd3cf140
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-05-25 11:40:15 +0200

    Added component to manipulate colors

---
+++ components/color/Slider.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <color-boxes :colors="hexValues" size="45px" :hover-effect="hoverEffect" />
+    <v-slider
+      v-model="sliderLevel"
+      :label="label"
+      :max="max"
+      :min="min"
+      thumb-label
+      :ticks="ticks"
+      append-icon="copy"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    hoverEffect: {
+      type: Boolean,
+      default: () => false
+    },
+    colors: {
+      type: Array,
+      default: () => []
+    },
+    type: {
+      type: String,
+      default: () => ''
+    },
+    label: {
+      type: String,
+      default: () => ''
+    },
+    max: {
+      type: Number,
+      default: () => 6
+    },
+    min: {
+      type: Number,
+      default: () => 0
+    },
+    step: {
+      type: Number,
+      default: () => 1
+    },
+    ticks: {
+      type: Boolean,
+      default: () => true
+    }
+  },
+  data() {
+    return {
+      sliderLevel: this.min
+    }
+  },
+  computed: {
+    hexValues() {
+      return this.colors.map(color => color[this.type](this.sliderLevel).hex())
+    }
+  }
+}
+</script>
