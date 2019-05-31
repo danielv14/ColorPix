062d28142bc88a341452e72bd26babcd6e6062c6
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-05-31 19:47:34 +0200

    Added core search field which pushes router to search layout component

---
+++ components/core/SearchField.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-text-field
+    v-model="input"
+    label="Search..."
+    single-line
+    append-icon="search"
+    class="searchbar"
+    @click:append="search"
+    @keyup.enter="search"
+  />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: ''
+    }
+  },
+  mounted() {
+    this.input = this.$route.query.query || ''
+  },
+  methods: {
+    search() {
+      this.$router.push({ name: 'search', query: { query: this.input } })
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.searchbar .v-input__slot
+  margin-bottom: 0px
+
+</style>
