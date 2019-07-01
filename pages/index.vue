14ad16be57a73f9fa3909c7047a66bfd96051290
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-07-01 19:17:01 +0200

    Added index contents (#23)

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,77 @@
 <template>
-  <v-layout>
-    <p>Index Page</p>
-  </v-layout>
+  <v-container fluid>
+    <div v-if="imagesPopular">
+      <h2 class="pl-4">
+        <v-icon>mdi-fire</v-icon>
+        Popular
+      </h2>
+      <v-layout align-start row wrap>
+        <v-flex
+          v-for="imagePopular in imagesPopular"
+          :key="imagePopular.id"  
+          xl4
+          md6
+          xs12
+          pa-4
+        >
+          <image-card :image="imagePopular" />
+        </v-flex>
+      </v-layout>
+      <v-layout v-if="imagesPopular" align-center justify-center>
+        <image-fetch-button text="Show more" :loading="loadingImages" @fetch="goTo('popular')" />
+      </v-layout>
+    </div>
+    <div v-if="imagesLatest">
+      <h2 class="pl-4">
+        <v-icon class="gray lighten-2">
+          mdi-history
+        </v-icon>
+        Latest
+      </h2>
+      <v-layout align-start row wrap>
+        <v-flex
+          v-for="imageLatest in imagesLatest"
+          :key="imageLatest.id"  
+          xl4
+          md6
+          xs12
+          pa-4
+        >
+          <image-card :image="imageLatest" />
+        </v-flex>
+      </v-layout>
+      <v-layout v-if="imagesLatest" align-center justify-center>
+        <image-fetch-button text="Show more" :loading="loadingImages" @fetch="goTo('latest')" />
+      </v-layout>
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
-  components: {}
+  data() {
+    return {
+      images: [],
+      imagesLatest: [],
+      imagesPopular: []
+    }
+  },
+  mounted() {
+    this.getPopularImages()
+    this.getLatestImages()
+  },
+  methods: {
+    async getLatestImages() {
+      const { data } = await this.$api.getImagesLatest({ perPage: 4 })
+      this.imagesLatest = data
+    },
+    async getPopularImages() {
+      const { data } = await this.$api.getImagesPopular({ perPage: 4 })
+      this.imagesPopular = data
+    },
+    goTo(route) {
+      this.$router.push({ name: 'list', query: { orderBy: route } })
+    }
+  }
 }
 </script>
