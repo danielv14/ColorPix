f6577c4bd2d964c08b1b0cf5b35246a615562e36
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-07-01 18:37:58 +0200

    Add copy to clipboard component (#21)
    * Added copy npm lib and clipboard copy component used alongside slider to copy colors to clipboard
    
    * Remove not neede component method. Copying is done elsewhere

--- components/color/Slider.vue
+++ components/color/Slider.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <color-boxes :colors="hexValues" size="45px" :hover-effect="hoverEffect" />
-    <v-slider
-      v-model="sliderLevel"
-      :label="label"
-      :max="max"
-      :min="min"
-      thumb-label
-      :ticks="ticks"
-      append-icon="copy"
-    />
+    <v-layout align-baseline>
+      <v-slider
+        v-model="sliderLevel"
+        :label="label"
+        :max="max"
+        :min="min"
+        thumb-label
+        :ticks="ticks"
+      />
+      <color-clipboard-copy :colors="hexValues" />
+    </v-layout>
   </div>
 </template>
 
