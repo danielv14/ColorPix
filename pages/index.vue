9af8a228f72db6eab068a1be79b89ccb5186fc91
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-07-30 19:07:52 +0200

    Prioritize latest images before popular on index page (#33)
    * Prioritize latest images before popular. Latest changes more often
    
    * Move latest above popular as nav object
    
    * Change default perPage setting for index page to 6

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,15 @@
 <template>
   <v-container fluid>
-    <div v-if="imagesPopular">
-      <h2 class="pl-4">
-        <v-icon>mdi-fire</v-icon>
-        Popular
-      </h2>
-      <v-layout align-start row wrap>
-        <v-flex
-          v-for="imagePopular in imagesPopular"
-          :key="imagePopular.id"  
-          xl4
-          md6
-          xs12
-          pa-4
-        >
-          <image-card :image="imagePopular" />
-        </v-flex>
-      </v-layout>
-      <v-layout v-if="imagesPopular" align-center justify-center>
-        <image-fetch-button text="Show more" @fetch="goTo('popular')" />
-      </v-layout>
-    </div>
     <div v-if="imagesLatest">
       <h2 class="pl-4">
         <v-icon class="gray lighten-2">
           mdi-clock-outline
-        </v-icon>
-        Latest
+        </v-icon>Latest
       </h2>
       <v-layout align-start row wrap>
         <v-flex
           v-for="imageLatest in imagesLatest"
-          :key="imageLatest.id"  
+          :key="imageLatest.id"
           xl4
           md6
           xs12
@@ -42,6 +20,26 @@
       </v-layout>
       <v-layout v-if="imagesLatest" align-center justify-center>
         <image-fetch-button text="Show more" @fetch="goTo('latest')" />
+      </v-layout>
+    </div>
+    <div v-if="imagesPopular">
+      <h2 class="pl-4">
+        <v-icon>mdi-fire</v-icon>Popular
+      </h2>
+      <v-layout align-start row wrap>
+        <v-flex
+          v-for="imagePopular in imagesPopular"
+          :key="imagePopular.id"
+          xl4
+          md6
+          xs12
+          pa-4
+        >
+          <image-card :image="imagePopular" />
+        </v-flex>
+      </v-layout>
+      <v-layout v-if="imagesPopular" align-center justify-center>
+        <image-fetch-button text="Show more" @fetch="goTo('popular')" />
       </v-layout>
     </div>
   </v-container>
@@ -61,11 +59,11 @@ export default {
   },
   methods: {
     async getLatestImages() {
-      const { data } = await this.$api.getImagesLatest({ perPage: 4 })
+      const { data } = await this.$api.getImagesLatest({ perPage: 6 })
       this.imagesLatest = data
     },
     async getPopularImages() {
-      const { data } = await this.$api.getImagesPopular({ perPage: 4 })
+      const { data } = await this.$api.getImagesPopular({ perPage: 6 })
       this.imagesPopular = data
     },
     goTo(route) {
