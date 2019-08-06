98a69c20d81b835ee60854ce2e11c40bd629aacc
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-08-06 22:10:42 +0200

    Revert to timeout wrapper

--- server/services/imageRandom.test.js
+++ server/services/imageRandom.test.js
@@ -1,5 +1,4 @@
 import { fetchRandomImage } from './imageRandom.service'
-jest.useFakeTimers()
 
 describe('ImageRandom.service', () => {
   it('should be defined', () => {
@@ -7,14 +6,12 @@ describe('ImageRandom.service', () => {
   })
 
   it('should be able to retrieve a random image', () => {
-    jest.runAllTimers()
-    const fn = async () => {
+    setTimeout(async () => {
       const image = await fetchRandomImage()
       const imageSecond = await fetchRandomImage()
       expect(image.id).toBeDefined()
       expect(image.description).toBeDefined()
       expect(image.id).not.toMatch(imageSecond.id)
-    }
-    return fn()
+    })
   })
 })
