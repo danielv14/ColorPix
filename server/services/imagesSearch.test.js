a115dcb6655b38e1c44585c1c83ae5ae02ae89ea
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-08-06 22:25:35 +0200

    Setup Travis CI to run jest test and build nuxt application (#46)
    * Fix test that failed due to latest and oldest list response from unsplash api contained the same image as pos 0
    Now test checkes latest and popular and cross reference the id's of the two lists
    
    * Add simple travis ci setup
    
    * cache npm and specify latest node version
    
    * Use jest.setup file to set the global timeout to avoid async/await callback timeout error
    
    * Wrap async tests in settimouts as a workaround
    
    * Add travis build badge to readme

--- server/services/imagesSearch.test.js
+++ server/services/imagesSearch.test.js
@@ -5,34 +5,42 @@ describe('ImageRandom.service', () => {
     expect(imagesSearch).toBeDefined()
   })
 
-  it('should be able to fetch default set of images for given keyword', async () => {
-    const images = await imagesSearch({ keyword: 'dogs' })
-    expect(images.results.length).toEqual(10)
-  })
-
-  it('should be able to set size of search result', async () => {
-    const images = await imagesSearch({ keyword: 'dogs', perPage: 5 })
-    expect(images.results.length).toEqual(5)
-  })
-
-  it('should be able to fetch multiple pages', async () => {
-    const imagesFirstPage = await imagesSearch({
-      keyword: 'dogs',
-      page: 1,
-      perPage: 10
+  it('should be able to fetch default set of images for given keyword', () => {
+    setTimeout(async () => {
+      const images = await imagesSearch({ keyword: 'dogs' })
+      expect(images.results.length).toEqual(10)
     })
-    const imagesSecondPage = await imagesSearch({
-      keyword: 'dogs',
-      page: 2,
-      perPage: 10
-    })
-    expect(imagesFirstPage.results[0].id).not.toMatch(
-      imagesSecondPage.results[0].id
-    )
   })
 
-  it('should return empty search result when no keyword is passed', async () => {
-    const images = await imagesSearch({ keyword: '' })
-    expect(images.results.length).toEqual(0)
+  it('should be able to set size of search result', () => {
+    setTimeout(async () => {
+      const images = await imagesSearch({ keyword: 'dogs', perPage: 5 })
+      expect(images.results.length).toEqual(5)
+    })
+  })
+
+  it('should be able to fetch multiple pages', () => {
+    setTimeout(async () => {
+      const imagesFirstPage = await imagesSearch({
+        keyword: 'dogs',
+        page: 1,
+        perPage: 10
+      })
+      const imagesSecondPage = await imagesSearch({
+        keyword: 'dogs',
+        page: 2,
+        perPage: 10
+      })
+      expect(imagesFirstPage.results[0].id).not.toMatch(
+        imagesSecondPage.results[0].id
+      )
+    })
+  })
+
+  it('should return empty search result when no keyword is passed', () => {
+    setTimeout(async () => {
+      const images = await imagesSearch({ keyword: '' })
+      expect(images.results.length).toEqual(0)
+    })
   })
 })
