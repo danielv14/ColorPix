126c4e9f1751835546950c47ccd9503093ad9f20
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-08-06 21:58:56 +0200

    Trying out jest fake timer testing

--- server/services/imageRandom.test.js
+++ server/services/imageRandom.test.js
@@ -1,4 +1,5 @@
 import { fetchRandomImage } from './imageRandom.service'
+jest.useFakeTimers()
 
 describe('ImageRandom.service', () => {
   it('should be defined', () => {
@@ -6,12 +7,14 @@ describe('ImageRandom.service', () => {
   })
 
   it('should be able to retrieve a random image', () => {
-    setTimeout(async () => {
+    jest.runAllTimers()
+    const fn = async () => {
       const image = await fetchRandomImage()
       const imageSecond = await fetchRandomImage()
       expect(image.id).toBeDefined()
       expect(image.description).toBeDefined()
       expect(image.id).not.toMatch(imageSecond.id)
-    })
+    }
+    return fn()
   })
 })
