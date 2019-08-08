e3ea2473eaaaeb0335f5c12d00c0e2f414d80a3d
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-08-08 18:07:40 +0200

    Remove settimeout from tests. They gave a false positive The problem with the tests were that travis did not have secret env keys

--- server/services/imageList.test.js
+++ server/services/imageList.test.js
@@ -5,40 +5,32 @@ describe('imageList.service', () => {
     expect(fetchImageList).toBeDefined()
   })
 
-  it('should be able to retrieve a list of the latest images', () => {
-    setTimeout(async () => {
-      const images = await fetchImageList()
-      expect(images.length).toBeGreaterThan(1)
-      expect(images[0]).toHaveProperty('id')
-    })
+  it('should be able to retrieve a list of the latest images', async () => {
+    const images = await fetchImageList()
+    expect(images.length).toBeGreaterThan(1)
+    expect(images[0]).toHaveProperty('id')
   })
 
-  it('should be able to set size of image list to retrieve', () => {
-    setTimeout(async () => {
-      const images = await fetchImageList({ perPage: 5 })
-      expect(images.length).toBe(5)
-    })
+  it('should be able to set size of image list to retrieve', async () => {
+    const images = await fetchImageList({ perPage: 5 })
+    expect(images.length).toBe(5)
   })
 
-  it('should be able to retrieve different pages', () => {
-    setTimeout(async () => {
-      const imagesFirstPage = await fetchImageList({ page: 1 })
-      const imagesSecondPage = await fetchImageList({ page: 2 })
-      expect(imagesFirstPage[0].id).not.toEqual(imagesSecondPage[0].id)
-    })
+  it('should be able to retrieve different pages', async () => {
+    const imagesFirstPage = await fetchImageList({ page: 1 })
+    const imagesSecondPage = await fetchImageList({ page: 2 })
+    expect(imagesFirstPage[0].id).not.toEqual(imagesSecondPage[0].id)
   })
 
-  it('shoulb be able to set sort order', () => {
-    setTimeout(async () => {
-      const imagesLatest = await fetchImageList({ orderBy: 'latest' })
-      const imagesPopular = await fetchImageList({ orderBy: 'popular' })
-      const latestIds = imagesLatest.map(image => image.id)
-      const popularIds = imagesPopular.map(image => image.id)
-      // Lists can sometimes contain the same image. Determine different ids by cross reference the id lists
-      const overlapIds = latestIds.map(id => popularIds.includes(id))
-      expect(
-        overlapIds.filter(overlap => overlap).length < latestIds.length
-      ).toBe(true)
-    })
+  it('shoulb be able to set sort order', async () => {
+    const imagesLatest = await fetchImageList({ orderBy: 'latest' })
+    const imagesPopular = await fetchImageList({ orderBy: 'popular' })
+    const latestIds = imagesLatest.map(image => image.id)
+    const popularIds = imagesPopular.map(image => image.id)
+    // Lists can sometimes contain the same image. Determine different ids by cross reference the id lists
+    const overlapIds = latestIds.map(id => popularIds.includes(id))
+    expect(
+      overlapIds.filter(overlap => overlap).length < latestIds.length
+    ).toBe(true)
   })
 })
--- utils/ImagePalette.test.js
+++ utils/ImagePalette.test.js
@@ -13,28 +13,22 @@ describe('ImagePalette class', () => {
     inheritedMethods.map(method => expect(imagePalette[method]).toBeDefined())
   })
 
-  it('should get dominant colors from an image', () => {
-    setTimeout(async () => {
-      const colors = await imagePalette.getDominantColors()
-      expect(colors.length).toBeGreaterThan(0)
-    })
+  it('should get dominant colors from an image', async () => {
+    const colors = await imagePalette.getDominantColors()
+    expect(colors.length).toBeGreaterThan(0)
   })
 
-  it('should get dominant colors in hex format', () => {
-    setTimeout(async () => {
-      const colors = await imagePalette.getDominantColors()
-      expect(colors[0].includes('#')).toBeTruthy()
-    })
+  it('should get dominant colors in hex format', async () => {
+    const colors = await imagePalette.getDominantColors()
+    expect(colors[0].includes('#')).toBeTruthy()
   })
 
-  it('should get dominant colors as Chroma instances', () => {
-    setTimeout(async () => {
-      const colors = await imagePalette.getChromaColors()
-      const color = colors[0]
-      // Test that colors has inherited chroma prototype
-      expect(color.hex).toBeDefined()
-      expect(color.rgb).toBeDefined()
-      expect(color.saturate).toBeDefined()
-    })
+  it('should get dominant colors as Chroma instances', async () => {
+    const colors = await imagePalette.getChromaColors()
+    const color = colors[0]
+    // Test that colors has inherited chroma prototype
+    expect(color.hex).toBeDefined()
+    expect(color.rgb).toBeDefined()
+    expect(color.saturate).toBeDefined()
   })
 })
