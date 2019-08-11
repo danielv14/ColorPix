c7c154b145c64d796656f7a81beb8dc3b3ed6588
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-08-11 19:42:59 +0200

    Refactor search tabs into own core component

---
+++ components/core/SearchNavigationTabs.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-tabs align-with-title class="search-tabs" background-color="transparent">
+    <v-tab nuxt exact :to="getRoute('images')">
+      Images
+    </v-tab>
+    <v-tab nuxt exact :to="getRoute('collections')">
+      Collections
+    </v-tab>
+  </v-tabs>
+</template>
+
+<script>
+export default {
+  methods: {
+    getRoute(name) {
+      return {
+        path: `/search/${name}`,
+        query: { keyword: this.$route.query.keyword || '' }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.search-tabs
+  justify-content: center
+  display: flex
+</style>
